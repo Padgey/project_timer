a6e48bd394e91414b4caec06fb730dcaa8db3d7c
Philip <philipadgey@gmail.com>
2021-04-29 09:55:41 +0100

    Tidied JS code

--- script.js
+++ script.js
@@ -1,4 +1,84 @@
-const timeToString = time => {
+//DOM Structure ///////////////////////////////////////////////
+const showCountdownButton = document.getElementById("showCountdownButton");
+const countdownDiv = document.getElementById("countdownDiv");
+const breakPromptDiv = document.getElementById("breakPrompt");
+const breakPromptStart = document.getElementById("breakPromptStart");
+const breakPromptPause = document.getElementById("breakPromptPause");
+const breakPromptReset = document.getElementById("breakPromptReset");
+
+const showToDoButton = document.getElementById("showToDoButton");
+const toDoDiv = document.getElementById("toDo");
+const toDoLi = document.getElementById('toDoLi');
+
+const showStudyCountdownButton = document.getElementById("showStudyCountdown");
+const studyCountdownDiv = document.getElementById("studyCountdownDiv");
+
+const sTotalTimer = document.getElementById("study_total_text");
+const sCurrentTimer = document.getElementById("study_current_text");
+const sPlayButton = document.getElementById("study_play");
+const sPauseButton = document.getElementById("study_pause");
+const sResetButton = document.getElementById("study_reset");
+const sAddMinuteButton = document.getElementById("splus_one_study");
+const sSubtractMinuteButton = document.getElementById("sminus_one_study");
+
+const pTotalTimer = document.getElementById("procrastination_total_text");
+const pCurrentTimer = document.getElementById("procrastination_current_text");
+const pPlayButton = document.getElementById("procrastination_play");
+const pPauseButton = document.getElementById("procrastination_pause");
+const pResetButton = document.getElementById("procrastination_reset");
+const pAddMinuteButton = document.getElementById("pplus_one_study");
+const pSubtractMinuteButton = document.getElementById("pminus_one_study");
+
+const bTotalTimer = document.getElementById("break_total_text");
+const bCurrentTimer = document.getElementById("break_current_text");
+const bPlayButton = document.getElementById("break_play");
+const bPauseButton = document.getElementById("break_pause");
+const bResetButton = document.getElementById("break_reset");
+const bAddMinuteButton = document.getElementById("bplus_one_study");
+const bSubtractMinuteButton = document.getElementById("bminus_one_study");
+
+const startStudyButton = document.getElementById('start_study');
+const startProcrastinationButton = document.getElementById('start_procrastination');
+const startBreakButton = document.getElementById('start_break');
+
+
+
+
+
+
+
+
+//Global Variables ////////////////////////////////////////////
+//CountdownBreakPromptVariables
+let initialTimeToPrompt = 1200000; //20 minutes - 1000*60*20
+let totalTimeToPrompt = initialTimeToPrompt;
+let countdownStatus = "Stopped";
+let countdownStartTime;
+let countdownElapsed = 0;
+let countdownInterval;
+let timeToPrompt;
+
+//Study Stopwatch
+let sStartTime;
+let sElapsedTime = 0;
+let sTimerInterval;
+let sTimeLeft;
+let sTotalTime = 0;
+
+//Procrastination Stopwatch
+let pStartTime;
+let pElapsedTime = 0;
+let pTimerInterval;
+let pTotalTime = 0;
+
+//Break Stopwatch
+let bStartTime;
+let bElapsedTime = 0;
+let bTimerInterval;
+let bTotalTime = 0;
+
+//Helper Functions ////////////////////////////////////////////
+function timeToString(time) {
     let diffInHrs = time / 3600000;
     let hh = Math.floor(diffInHrs);
 
@@ -14,23 +94,11 @@ const timeToString = time => {
 
     return `${formattedHH}:${formattedMM}:${formattedSS}`;
 };
-
 function setTitle() {
-    document.title = timeToString(sTimeLeft - sElapsedTime)
+    document.title = timeToString(sTotalTime + sElapsedTime);
 };
 
-const showCountdownButton = document.getElementById("showCountdownButton");
-const showStudyCountdownButton = document.getElementById("showStudyCountdown");
-const showToDoButton = document.getElementById("showToDoButton");
-const breakPromptDiv = document.getElementById("breakPrompt");
-const countdownDiv = document.getElementById("countdownDiv");
-const studyCountdownDiv = document.getElementById("studyCountdownDiv");
-const toDoDiv = document.getElementById("toDo");
-
-countdownDiv.style.display = "none";
-toDoDiv.style.display = "none"; //To-Do list not displayed by default
-
-//Display Controls
+//Display Controls ////////////////////////////////////////////
 function showCountdown() {
     if (countdownDiv.style.display === "none") {
         countdownDiv.style.display = "block"
@@ -38,42 +106,22 @@ function showCountdown() {
         countdownDiv.style.display = "none"
     }
 };
-
-function showStudyCountdown() {
-    if (studyCountdownDiv.style.display === "none") {
-        studyCountdownDiv.style.display = "block"
-    } else {
-        studyCountdownDiv.style.display = "none"
-    }
-}
-
 function showToDo() {
     if (toDoDiv.style.display === "none") {
         toDoDiv.style.display = "block"
     } else {
         toDoDiv.style.display = "none"
     }
-}
-
-showCountdownButton.addEventListener("click", showCountdown);
-showStudyCountdownButton.addEventListener("click", showStudyCountdown);
-showToDoButton.addEventListener("click", showToDo);
-
-
-
-//Break prompt
-
-
-let initialTimeToPrompt = 1200000; //20 minutes - 1000*60*20
-let totalTimeToPrompt = initialTimeToPrompt;
-let countdownStatus = "Stopped";
-let countdownStartTime;
-let countdownElapsed = 0;
-let countdownInterval;
-let timeToPrompt;
-
+};
+function showStudyCountdown() {
+    if (studyCountdownDiv.style.display === "block") {
+        studyCountdownDiv.style.display = "none"
+    } else {
+        studyCountdownDiv.style.display = "block"
+    }
+};
+//CountdownBreakPrompt ////////////////////////////////////////
 breakPromptDiv.innerText = timeToString(initialTimeToPrompt);
-
 
 function countdownPrint(txt) {
     if (timeToPrompt > 0) {
@@ -81,8 +129,7 @@ function countdownPrint(txt) {
     } else {
         breakPromptDiv.innerText = "Time to stretch and rest your eyes!"
     }
-}
-
+};
 function countdownTimer() {
     if (countdownStatus === "Stopped") {
         countdownStartTime = Date.now();
@@ -101,9 +148,7 @@ function countdownTimer() {
     } else {
         countdownErrorDiv.innerText = "This error should not appear!";
     }
-   
 };
-
 function countdownPause() {
     clearInterval(countdownInterval);
     countdownStatus = "Stopped";
@@ -111,7 +156,6 @@ function countdownPause() {
     totalTimeToPrompt = totalTimeToPrompt - countdownElapsed;
     countdownElapsed = 0;
 };
-
 function countdownStop() {
     clearInterval(countdownInterval);
     countdownStatus = "Stopped";
@@ -119,7 +163,6 @@ function countdownStop() {
     totalTimeToPrompt = totalTimeToPrompt - countdownElapsed;
     countdownElapsed = 0;
 };
-
 function countdownReset() {
     clearInterval(countdownInterval);
     countdownStatus = "Stopped";
@@ -128,36 +171,15 @@ function countdownReset() {
     timeToPrompt = initialTimeToPrompt;
     countdownElapsed = 0;
     countdownPrint(timeToString(totalTimeToPrompt));
-}
+};
 
-const breakPromptStart = document.getElementById("breakPromptStart");
-const breakPromptPause = document.getElementById("breakPromptPause");
-const breakPromptReset = document.getElementById("breakPromptReset");
-
-breakPromptStart.addEventListener("click", countdownTimer);
-breakPromptPause.addEventListener("click", countdownPause);
-breakPromptReset.addEventListener("click", countdownReset);
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-//Study stopwatch
-const sTotalTimer = document.getElementById("study_total_text");
-const sCurrentTimer = document.getElementById("study_current_text");
-
-let sStartTime;
-let sElapsedTime = 0;
-let sTimerInterval;
-let sTimeLeft;
-let sTotalTime = 0;
-
-
+//Study Stopwatch /////////////////////////////////////////////
 function sPrint(txt) {
     sCurrentTimer.innerHTML = txt;
 };
-
 function sTotalPrint(txt) {
     sTotalTimer.innerHTML = txt;
 };
-
 function sStart() {
     sStartTime = Date.now() - sElapsedTime;
     
@@ -168,11 +190,9 @@ function sStart() {
         setTitle();
     }, 100);
 };
-
 function sPause() {
     clearInterval(sTimerInterval)
 };
-
 function sReset() {
     clearInterval(sTimerInterval);
     sPrint("00:00:00");
@@ -180,59 +200,22 @@ function sReset() {
     sTotalPrint(timeToString(sTotalTime));
     sElapsedTime = 0;
 };
-
-
-const sPlayButton = document.getElementById("study_play");
-const sPauseButton = document.getElementById("study_pause");
-const sResetButton = document.getElementById("study_reset");
-
-
-sPlayButton.addEventListener("click", sStart);
-sPauseButton.addEventListener("click", sPause);
-sResetButton.addEventListener("click", sReset);
-
-//Add minute
-const sAddMinuteButton = document.getElementById("splus_one_study");
-
 function sAddMinute() {
     sTimeLeft = sTimeLeft - 60000;
     sTotalPrint(timeToString(sTimeLeft));
 };
-
-sAddMinuteButton.addEventListener("click", sAddMinute);
-
-//Subtract minute
-const sSubtractMinuteButton = document.getElementById("sminus_one_study");
-
 function sSubtractMinute() {
     sTimeLeft = sTimeLeft + 60000;
     sTotalPrint(timeToString(sTimeLeft));
 };
 
-sSubtractMinuteButton.addEventListener("click", sSubtractMinute);
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////
-//Procrastination stopwatch
-const pTotalTimer = document.getElementById("procrastination_total_text");
-const pCurrentTimer = document.getElementById("procrastination_current_text");
-
-let pStartTime;
-let pElapsedTime = 0;
-let pTimerInterval;
-let pTotalTime = 0;
-
-
+//Procrastination Stopwatch ///////////////////////////////////
 function pPrint(txt) {
     pCurrentTimer.innerHTML = txt;
 };
-
 function pTotalPrint(txt) {
     pTotalTimer.innerHTML = txt;
 };
-
 function pStart() {
     pStartTime = Date.now() - pElapsedTime;
     pTimerInterval = setInterval(function printTime() {
@@ -242,11 +225,9 @@ function pStart() {
         setTitle();
     }, 100);
 };
-
 function pPause() {
     clearInterval(pTimerInterval)
 };
-
 function pReset() {
     clearInterval(pTimerInterval);
     pPrint("00:00:00");
@@ -254,31 +235,10 @@ function pReset() {
     pTotalPrint(timeToString(pTotalTime));
     pElapsedTime = 0;
 };
-
-
-const pPlayButton = document.getElementById("procrastination_play");
-const pPauseButton = document.getElementById("procrastination_pause");
-const pResetButton = document.getElementById("procrastination_reset");
-
-
-pPlayButton.addEventListener("click", pStart);
-pPauseButton.addEventListener("click", pPause);
-pResetButton.addEventListener("click", pReset);
-
-
-//Add minute
-const pAddMinuteButton = document.getElementById("pplus_one_study");
-
 function pAddMinute() {
     pTotalTime = pTotalTime + 60000;
     pTotalPrint(timeToString(pTotalTime));
 };
-
-pAddMinuteButton.addEventListener("click", pAddMinute);
-
-//Subtract minute
-const pSubtractMinuteButton = document.getElementById("pminus_one_study");
-
 function pSubtractMinute() {
     if (pTotalTime >= 60000) {
     pTotalTime = pTotalTime - 60000;
@@ -288,28 +248,13 @@ function pSubtractMinute() {
     pTotalPrint(timeToString(pTotalTime));
 };
 
-pSubtractMinuteButton.addEventListener("click", pSubtractMinute);
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////
-//Break stopwatch
-const bTotalTimer = document.getElementById("break_total_text");
-const bCurrentTimer = document.getElementById("break_current_text");
-
-let bStartTime;
-let bElapsedTime = 0;
-let bTimerInterval;
-let bTotalTime = 0;
-
-
+//Break Stopwatch /////////////////////////////////////////////
 function bPrint(txt) {
     bCurrentTimer.innerHTML = txt;
 };
-
 function bTotalPrint(txt) {
     bTotalTimer.innerHTML = txt;
 };
-
 function bStart() {
     bStartTime = Date.now() - bElapsedTime;
     bTimerInterval = setInterval(function printTime() {
@@ -319,11 +264,9 @@ function bStart() {
         setTitle();
     }, 100);
 };
-
 function bPause() {
     clearInterval(bTimerInterval)
 };
-
 function bReset() {
     clearInterval(bTimerInterval);
     bPrint("00:00:00");
@@ -331,30 +274,10 @@ function bReset() {
     bTotalPrint(timeToString(bTotalTime));
     bElapsedTime = 0;
 };
-
-
-const bPlayButton = document.getElementById("break_play");
-const bPauseButton = document.getElementById("break_pause");
-const bResetButton = document.getElementById("break_reset");
-
-
-bPlayButton.addEventListener("click", bStart);
-bPauseButton.addEventListener("click", bPause);
-bResetButton.addEventListener("click", bReset);
-
-//Add minute
-const bAddMinuteButton = document.getElementById("bplus_one_study");
-
 function bAddMinute() {
     bTotalTime = bTotalTime + 60000;
     bTotalPrint(timeToString(bTotalTime));
 };
-
-bAddMinuteButton.addEventListener("click", bAddMinute);
-
-//Subtract minute
-const bSubtractMinuteButton = document.getElementById("bminus_one_study");
-
 function bSubtractMinute() {
     if (bTotalTime >= 60000) {
     bTotalTime = bTotalTime - 60000;
@@ -364,53 +287,27 @@ function bSubtractMinute() {
     bTotalPrint(timeToString(bTotalTime));
 };
 
-bSubtractMinuteButton.addEventListener("click", bSubtractMinute);
-
-///////////////////////////////////////////////////////////////////////////////////////////
-//Bottom button functions
-const startStudyButton = document.getElementById('start_study');
-const startProcrastinationButton = document.getElementById('start_procrastination');
-const startBreakButton = document.getElementById('start_break');
-
-const startStudy = () => {
+//Start Buttons ///////////////////////////////////////////////
+function startStudy() {
     pReset();
     bReset();
     sReset();
     sStart();
-}
-
-startStudyButton.onclick = function() {
-    startStudy()
-}
-
-const startProcrastination = () => {
+};
+function startProcrastination() {
     sReset();
     bReset();
     pReset();
     pStart();
-}
-
-
-startProcrastinationButton.onclick = function() {
-    startProcrastination()
-}
-
-const startBreak = () => {
+};
+function startBreak() {
     pReset();
     sReset();
     bReset();
     bStart();
-}
+};
 
-
-startBreakButton.onclick = function() {
-    startBreak()
-}
-
-
-///////////////////////////////////////////////////////////////////////////
-const toDoLi = document.getElementById('toDoLi');
-
+//To-Do List //////////////////////////////////////////////////
 function addNewElement() {
     let li = document.createElement("li");
     let inputValue = document.getElementById("inputToDo").value;
@@ -437,61 +334,46 @@ function addNewElement() {
             let div = this.parentElement;
             div.style.display = "none";
         }
-    }
-}
+    };
+};
 
+//Event Listeners /////////////////////////////////////////////
+showCountdownButton.addEventListener("click", showCountdown);
+
+showToDoButton.addEventListener("click", showToDo);
 document.querySelector('#inputToDo').addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
       addNewElement()
     }
 });
 
+showStudyCountdownButton.addEventListener("click", showStudyCountdown);
 
-//////////////////////////////////////////////////////////////////////////////////////////
-//JS testing - Making sure JS file is working correctly
+breakPromptStart.addEventListener("click", countdownTimer);
+breakPromptPause.addEventListener("click", countdownPause);
+breakPromptReset.addEventListener("click", countdownReset);
 
-/*
-startStudyButton.onclick = function() {
-    startStudy.style.backgroundColor = 'red';
-}
-*/
+sPlayButton.addEventListener("click", sStart);
+sPauseButton.addEventListener("click", sPause);
+sResetButton.addEventListener("click", sReset);
+sAddMinuteButton.addEventListener("click", sAddMinute);
+sSubtractMinuteButton.addEventListener("click", sSubtractMinute);
 
+pPlayButton.addEventListener("click", pStart);
+pPauseButton.addEventListener("click", pPause);
+pResetButton.addEventListener("click", pReset);
+pAddMinuteButton.addEventListener("click", pAddMinute);
+pSubtractMinuteButton.addEventListener("click", pSubtractMinute);
 
+bPlayButton.addEventListener("click", bStart);
+bPauseButton.addEventListener("click", bPause);
+bResetButton.addEventListener("click", bReset);
+bAddMinuteButton.addEventListener("click", bAddMinute);
+bSubtractMinuteButton.addEventListener("click", bSubtractMinute);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+startStudyButton.addEventListener("click", startStudy);
+startProcrastinationButton.addEventListener("click", startProcrastination);
+startBreakButton.addEventListener("click", startBreak);
 
 
 
-
-
-/*
-const pTotalTimer = getElementById("procrastination_total");
-const pCurrentTimer = getElementId("procrastination_current");
-
-const pPlay = getElementById("procrastination_play");
-const pPause = getElementById("procrastination_pause");
-const pReset = getElementById("procrastination_reset");
-
-const bTotalTimer = getElementById("break_total");
-const bCurrentTimer = getElementId("break_current");
-
-const bPlay = getElementById("break_play");
-const bPause = getElementById("break_pause");
-const bReset = getElementById("break_reset");
-*/
